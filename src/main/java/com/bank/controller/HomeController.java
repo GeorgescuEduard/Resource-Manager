e362181dddbde10c6c4cc5b0f7b87b2801f08ee5
Eduard <georgescu.eduard@outlook.com>
2021-01-18 21:32:19 +0200

    Update HomeController.java

--- src/main/java/com/bank/controller/HomeController.java
+++ src/main/java/com/bank/controller/HomeController.java
@@ -3,7 +3,12 @@ package com.bank.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class HomeController {
-
+    @RequestMapping("/")
+    public String index(Map<String, Object> model) {
+        return "redirect:/register";
+    }
 }
